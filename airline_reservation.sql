eb943a3379255a322e974ba326bad2ba7cfedc81
schau-sjsu <78131171+schau-sjsu@users.noreply.github.com>
2021-11-08 14:23:12 -0800

    Add triggers

--- airline_reservation.sql
+++ airline_reservation.sql
@@ -55,10 +55,6 @@ create table Reservations
  ticketCost float
 );
 
-# Constraints
-
-
-
 # Triggers
 
 ## If baggage weight is over limit, charge extra on ticket
@@ -69,9 +65,57 @@ create trigger overweightBagCharge
 after insert on reservation
 for each row
 begin
-	if (select totalBagWeight from passenger where new.uid = passenger.uid) > 50 then
-		update reservation set ticketCost = ticketCost + 50 where rid = new.rid;
-	end if;
+    if (select totalBagWeight from passenger where new.uid = passenger.uid) > 50 then
+        update reservation set ticketCost = ticketCost + 50 where rid = new.rid;
+    end if;
+end;
+//
+delimiter ;
+
+## Update ticket price if passenger gets pushed down in class
+
+drop trigger if exists ticketChange;
+delimiter //
+create trigger ticketChange
+after update on reservations
+for each row
+begin
+    if new.ticketType > old.ticketType then
+    update reservation set ticketCost = ticketCost - (new.ticketType - old.ticketType) * 100  where rid = old.rid;
+    end if;
+end;
+//
+delimiter ;
+
+## Update ticket type if desired section is already full (next highest class is checked for room first before lowest/highest class for downgrades/upgrades)
+
+drop trigger if exists seatChange;
+delimiter //
+create trigger seatChange
+after insert on reservations
+for each row
+begin
+    if new.ticketType = 2 and (select count(*) from reservations where fid = new.fid and ticketType = 2) = (select numEcon from planes natural join flights where fid = new.fid) then
+		if (select count(*) from reservations where fid = new.fid and ticketType = 1) < (select numBusiness from planes natural join flights where fid = new.fid) then
+			update reservation set ticketType = 1 where rid = old.rid;
+		else
+			update reservation set ticketType = 0 where rid = old.rid;
+		end if;
+    end if;
+    if new.ticketType = 1 and (select count(*) from reservations where fid = new.fid and ticketType = 1) = (select numBusiness from planes natural join flights where fid = new.fid) then
+		if (select count(*) from reservations where fid = new.fid and ticketType = 0) < (select numFirst from planes natural join flights where fid = new.fid) then
+			update reservation set ticketType = 0 where rid = old.rid;
+		else
+			update reservation set ticketType = 2 where rid = old.rid;
+		end if;
+    end if;
+    if new.ticketType = 0 and (select count(*) from reservations where fid = new.fid and ticketType = 0) = (select numFirst from planes natural join flights where fid = new.fid) then
+		if (select count(*) from reservations where fid = new.fid and ticketType = 1) < (select numBusiness from planes natural join flights where fid = new.fid) then
+			update reservation set ticketType = 1 where rid = old.rid;
+		else
+			update reservation set ticketType = 2 where rid = old.rid;
+		end if;
+    end if;
 end;
 //
 delimiter ;
